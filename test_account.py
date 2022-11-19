b3378ff9ef3432a3dc0b96c1ccf209fa38a4c1c1
Will <116966613+WillKuhfahl@users.noreply.github.com>
2022-11-18 21:37:58 -0600

    First file

--- test_account.py
+++ test_account.py
@@ -1,1 +1,19 @@
-# ooga booga
+import unittest
+import account
+
+class TestAccount(unittest.TestCase):
+    def test_deposit(self):
+        self.assertEqual(account.Account.deposit)
+
+
+
+
+
+
+
+
+
+
+
+if __name__ == "__main__":
+    unittest.main()
