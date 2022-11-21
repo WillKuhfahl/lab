7b98b014fc9f1bbf685b747d7b740272c2fd4b8f
Will <116966613+WillKuhfahl@users.noreply.github.com>
2022-11-20 21:53:05 -0600

    Update account.py

--- account.py
+++ account.py
@@ -3,24 +3,14 @@ class Account:
         self.account_name = Account('001-Will')
         self.account_balance = 0
 
-    def deposit(self, amount) -> float:
-        """
-        Function to add an amount to the user's balance
-        :param amount: amount that the user wants to add
-        :return: returns true if the transaction is sucessful (value > 0), and false if not
-        """
+    def deposit(self, amount):
         if amount > 0:
             self.account_balance = self.account_balance + amount
             return True
         else:
             return False
 
-    def withdraw(self, amount) -> float:
-        """
-        Function to withdraw an amount from the user's balance
-        :param amount: amount that the user want to withdraw
-        :return: returns true if the transaction is sucessful (value > 0), and false if not
-        """
+    def withdraw(self, amount):
         if int(amount) > 0:
             self.account_balance = self.account_balance - amount
             return True
@@ -28,15 +18,7 @@ class Account:
             return False
 
     def get_balance(self):
-        """
-        Function to check the overall balance
-        :return: returns the account balance
-        """
         return self.account_balance
 
     def get_name(self):
-        """
-        Function to check the name of the account
-        :return: returns the account name
-        """
         return self.account_name
